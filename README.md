26f027dcb9d60a84dfd57e1d6f94bc9205eac94c
faelau <92048093+faelau@users.noreply.github.com>
2025-09-05 03:59:22 +0200

    feat: modify endpoint

---
+++ README.md
@@ -0,0 +1,155 @@
+# Navidrome ListenBrainz Playlist Importer
+
+This repository contains a plugin for fetching playlists from [ListenBrainz](https://listenbrainz.org/). It is a slightly modified version of [github.com/kgarner7/navidrome-listenbrainz-daily-playlist](https://github.com/kgarner7/navidrome-listenbrainz-daily-playlist).
+
+## Requirements
+1. Your library should have MBIDs for your tracks (or at least, most of them). This plugin does song lookups _only_ using MBID. For better fallback, having artist MBIDs will also help.
+2. A ListenBrainz account per user you wish to fetch
+3. Navidrome >= 0.58.0. 0.58.0 introduces the ability to get current time, which is required for this version.
+
+## Install instructions
+
+### From GitHub Release
+
+You can download the `listenbrainz-playlists.ndp` from the latest release and then run `navidrome plugin install listenbrainz-playlists.ndp`.
+Make sure to run this command as your navidrome user.
+This will unzip the package, and install it automatically in your plugin directory.
+
+### From source
+
+Requirements:
+- `go` 1.24
+
+#### Build WASM plugin
+
+```bash
+go mod download
+GOOS=wasip1 GOARCH=wasm go build -buildmode=c-shared -o plugin.wasm plugin.go
+```
+
+#### Package plugin
+
+Copy the following files: `manifest.json` and `plugin.wasm`. 
+Put them in a directory in your Navidrome `Plugins.Folder`.
+Make sure that:
+1. You have plugins enabled (`Plugins.Enabled = true`, `ND_PLUGINS_ENABLED = true`).
+2. Your Navidrome user has read permissions in the plugin directory
+
+Restart Navidrome and you should see the following message in your logs (`info` or more verbose)
+
+```
+level=info msg="Discovered plugin" capabilities="[LifecycleManagement SchedulerCallback]" dev_mode=false folder=listenbrainz-playlists name=listenbrainz-playlists wasm=PATH_TO_WASM_PLUGIN
+```
+
+## Configuration
+
+```toml
+# A complete configuration
+
+[Plugins]
+Enabled = true
+# Optional, if you want to specify a different path. Otherwise it is your data directory / plugins
+Folder = "SOME_CUSTOM_PATH_TO_PLUGINS"
+
+[PluginConfig.listenbrainz-daily-playlist]
+Split = ";"
+Schedule = "@every 24h"
+
+Sources = "https://listenbrainz.org/playlist/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxx;https://listenbrainz.org/playlist/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxx"
+"Sources[0]" = "ListenBrainz Daily Jams"
+"Sources[1]" = "ListenBrainz Weekly Jams"
+
+Users = "user1;user2"
+"Users[0]" = "lb-uzername-1"
+"Users[1]" = "lb-uzername-2"
+```
+
+The configuration has four main blocks:
+
+### Split
+This is an optional section, but it defines how multi-valued fields are split.
+The default value if not specified is `;`
+
+### Schedule
+This is a schedule instructing how often to run the sync.
+The sample configuration `@every 24h` does it every 24 hours.
+This is the default option, so it can be omitted.
+Please note that ListenBrainz playlists are only updated once a day.
+
+Also note, when Navidrome is restarted, this plugin will check if any playlists are out of date, and if so, fetch them.
+
+### Sources
+This specifies the source(s) to fetch from ListenBrainz, and what name to use when importing into Navidrome.
+Source names include:
+
+- `daily-jams`: daily playlist of tracks you've listened to before
+- `weekly-jams`: weekly playlist of tracks you've listened to before
+- `weekly-exploration`: weekly playlist of new tracks. This is likely to have few matches in your library
+
+`Sources` itself is multi-valued, split by the `Split` token.
+So, in the example above, you have two sources: `daily-jams` and `weekly-jams`.
+
+For each source, you must then specify the name to use when importing.
+The key is `sources[zero-based-index]`, and the value is the playlist name.
+
+In the above example, this means:
+1. Import `daily-jams` playlists with the name `ListenBrainz Daily Jams`
+2. Import `weekly-jams` playlists with the name `ListenBrainz Weekly Jams`
+
+### Users
+This specifies which Subsonic/Navidrome users to fetch, and provides their ListenBrainz username.
+
+In the example provided, there are two users, `user1` and `user`. 
+Their ListenBrainz usernames are `lb-uzername-1` and `lb-uzername-2`, respectively.
+
+### Optional settings
+
+#### Disable cehcking on startup
+To disable fetching on service start, pass in the following config to the plugin:
+
+```toml
+CheckOnStartup = "false"
+```
+
+Note that this value needs to be in quotes (not a boolean).
+
+
+#### Only include songs with a specific rating
+To only import songs with a given rating, you can pass in `rating[idx]` as a comma-separated value of ratings.
+Note that `0` means no rating
+
+```toml
+Users = "user1;user2"
+"Users[0]" = "lb-uzername-1"
+
+# Exclude songs with a 1-star rating
+"Rating[0]" = "0,2,3,4,5"
+```
+
+#### Specify number of tracks to fetch with fallback
+```toml
+FallbackCount = "15"
+```
+
+When searching by title when no specific match is found, this limits the number of potential track matches to search by name.
+By default, this value is 15, but may be decreased to 1 or increased up to 500.
+
+
+## How does it work?
+This plugin relies on a special quick of Navidrome, wherein using the `/rest/search3` endpoint by MBID will return exact matches.
+So, for most matches, you will want to make sure all your files have MBIDs.
+
+In the event that there is no match by track MBID (for example, ListenBrainz returns a _different_ track ID than the one you have), then the lookup process is as follows:
+1. Map every artist MBID in the track provided by ListenBrainz to a Subsonic artist ID. This is similarly done through `/rest/search3`, but for artists instead of tracks.
+2. Search for tracks with the name given by ListenBrainz.
+3. Find a track whose name is an **exact match** of the ListenBrainz track, and whose artists **all** match the artist IDS found in step 1.
+
+This fallback is of course not perfect.
+If your track name doesn't match **exactly** what ListenBrainz provides, or one or more of the artists doesn't have an MBID, then it will not be found.
+
+
+## Permissions
+- `config`: This plugin needs to read the config to determine what users to fetch, and so on
+- `http`: make requests to ListenBrainz API to fetch playlists
+- `scheduler`: schedule periodic task
+- `subsonicapi`, all users and admin: while this plugin requests access to all users, it only needs permissions for users specified in the configuration. It uses the following endpoints: `search3`, `getPlaylists`, `createPlaylist`, and `updatePlaylist`
---
+++ plugin.go
@@ -0,0 +1,654 @@
+//go:build wasip1
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"html"
+	"log"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/navidrome/navidrome/plugins/api"
+	"github.com/navidrome/navidrome/plugins/host/config"
+	"github.com/navidrome/navidrome/plugins/host/http"
+	"github.com/navidrome/navidrome/plugins/host/scheduler"
+	"github.com/navidrome/navidrome/plugins/host/subsonicapi"
+	"github.com/navidrome/navidrome/server/subsonic/responses"
+)
+
+const (
+	lbzEndpoint    = "https://api.listenbrainz.org/1"
+	peridiocSyncId = "listenbrainz"
+)
+
+var (
+	client = http.NewHttpService()
+
+	// SubsonicAPIService instance for making API calls
+	subsonicService = subsonicapi.NewSubsonicAPIService()
+)
+
+type BrainzPlaylistPlugin struct {
+	artistMbidToId map[string]string
+}
+
+type listenBrainzResponse struct {
+	Code          int               `json:"code"`
+	Message       string            `json:"message"`
+	Error         string            `json:"error"`
+	Status        string            `json:"status"`
+	Valid         bool              `json:"valid"`
+	UserName      string            `json:"user_name"`
+	PlaylistCount int               `json:"playlist_count"`
+	Playlists     []overallPlaylist `json:"playlists,omitempty"`
+	Playlist      *lbPlaylist       `json:"playlist,omitempty"`
+}
+
+type overallPlaylist struct {
+	Playlist lbPlaylist `json:"playlist"`
+}
+
+type lbPlaylist struct {
+	Annotation string       `json:"annotation"`
+	Creator    string       `json:"creator"`
+	Date       time.Time    `json:"date"`
+	Identifier string       `json:"identifier"`
+	Title      string       `json:"title"`
+	Extension  plsExtension `json:"extension"`
+	Tracks     []lbTrack    `json:"track"`
+}
+
+type plsExtension struct {
+	Extension playlistExtension `json:"https://musicbrainz.org/doc/jspf#playlist"`
+}
+
+type playlistExtension struct {
+	AdditionalMetadata additionalMeta `json:"additional_metadata"`
+	LastModified       time.Time      `json:"last_modified_at"`
+	Public             bool           `json:"public"`
+}
+
+type additionalMeta struct {
+	// AlgorithmMetadata algoMeta `json:"algorithm_metadata"`
+	CoverArt coverArt `json:"cover_art"`
+}
+
+// type algoMeta struct {
+// 	SourcePatch string `json:"source_patch"`
+// }
+
+type coverArt struct {
+	Dimension int `json:"dimension"`
+	Layout    int `json:"layout"`
+}
+
+type lbTrack struct {
+	Creator   string `json:"creator"`
+	Extension struct {
+		Track struct {
+			AdditionalMetadata struct {
+				Artists []struct {
+					MBID string `json:"artist_mbid"`
+				} `json:"artists"`
+			} `json:"additional_metadata"`
+		} `json:"https://musicbrainz.org/doc/jspf#track"`
+	} `json:"extension"`
+	Identifier []string `json:"identifier"`
+	Title      string   `json:"title"`
+}
+
+func getIdentifier(url string) string {
+	split := strings.Split(url, "/")
+	return split[len(split)-1]
+}
+
+func (b *BrainzPlaylistPlugin) getPlaylists(ctx context.Context, user string) ([]overallPlaylist, error) {
+	req := &http.HttpRequest{
+		Url: fmt.Sprintf("%s/user/%s/playlists", lbzEndpoint, user),
+		Headers: map[string]string{
+			"Accept":     "application/json",
+			"User-Agent": "NavidromePlaylistImporter/0.1",
+		},
+	}
+
+	resp, err := client.Get(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	var result listenBrainzResponse
+	if err := json.Unmarshal(resp.Body, &result); err != nil {
+		return nil, fmt.Errorf("Failed to decode JSON: %v", err)
+	}
+
+	if result.Code != 0 {
+		return nil, fmt.Errorf("ListenBrainz HTTP Error. Code: %d, Error: %s", result.Code, result.Error)
+	}
+
+	return result.Playlists, nil
+}
+
+func (b *BrainzPlaylistPlugin) getPlaylist(ctx context.Context, id string) (*lbPlaylist, error) {
+	req := &http.HttpRequest{
+		Url: fmt.Sprintf("%s/playlist/%s", lbzEndpoint, id),
+		Headers: map[string]string{
+			"Accept":     "application/json",
+			"User-Agent": "NavidromePlaylistImporter/0.1",
+		},
+	}
+
+	resp, err := client.Get(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	var result listenBrainzResponse
+	if err := json.Unmarshal(resp.Body, &result); err != nil {
+		return nil, fmt.Errorf("Failed to decode JSON: %v", err)
+	}
+
+	if result.Code != 0 {
+		return nil, fmt.Errorf("ListenBrainz HTTP Error. Code: %d, Error: %s", result.Code, result.Error)
+	}
+
+	if result.Playlist == nil {
+		return nil, fmt.Errorf("Nothing parsed for playlist %s", id)
+	}
+
+	return result.Playlist, nil
+}
+
+func (b *BrainzPlaylistPlugin) makeSubsonicRequest(ctx context.Context, endpoint, subsonicUser string, params *url.Values) (*responses.JsonWrapper, bool) {
+	subsonicResp, err := subsonicService.Call(ctx, &subsonicapi.CallRequest{
+		Url: fmt.Sprintf("/rest/%s?u=%s&%s", endpoint, subsonicUser, params.Encode()),
+	})
+
+	if err != nil {
+		log.Printf("An error occurred %s: %v", subsonicUser, err)
+		return nil, false
+	}
+
+	if subsonicResp.Error != "" {
+		log.Printf("A Subsonic error occurred for user %s: %s", subsonicUser, subsonicResp.Error)
+		return nil, false
+	}
+
+	var decoded responses.JsonWrapper
+	if err := json.Unmarshal([]byte(subsonicResp.Json), &decoded); err != nil {
+		log.Printf("A deserialization error occurred %s: %s", subsonicUser, err)
+		return nil, false
+	}
+
+	if decoded.Subsonic.Status != "ok" {
+		log.Printf("Subsonic status is not ok: (%d)%s", decoded.Subsonic.Error.Code, decoded.Subsonic.Error.Message)
+		return nil, false
+	}
+
+	return &decoded, true
+}
+
+func (b *BrainzPlaylistPlugin) findExistingPlaylist(resp *responses.JsonWrapper, playlistName string) *responses.Playlist {
+	if len(resp.Subsonic.Playlists.Playlist) > 0 {
+		for _, playlist := range resp.Subsonic.Playlists.Playlist {
+			if playlist.Name == playlistName {
+				return &playlist
+			}
+		}
+	}
+
+	return nil
+}
+
+func (b *BrainzPlaylistPlugin) findArtistIdByMbid(
+	ctx context.Context,
+	subsonicUser string,
+	mbid string,
+) string {
+	existing, ok := b.artistMbidToId[mbid]
+	if ok {
+		return existing
+	}
+
+	artistParams := url.Values{
+		"artistCount": []string{"1"},
+		"albumCount":  []string{"0"},
+		"songCount":   []string{"0"},
+		"query":       []string{mbid},
+	}
+
+	resp, ok := b.makeSubsonicRequest(ctx, "search3", subsonicUser, &artistParams)
+	if !ok {
+		return ""
+	}
+
+	var id string
+
+	if len(resp.Subsonic.SearchResult3.Artist) > 0 {
+		id = resp.Subsonic.SearchResult3.Artist[0].Id
+	} else {
+		id = ""
+	}
+
+	b.artistMbidToId[mbid] = id
+	return id
+}
+
+func (b *BrainzPlaylistPlugin) fallbackLookup(
+	ctx context.Context,
+	subsonicUser string,
+	track *lbTrack,
+	fallbackCount string,
+) *responses.Child {
+	artistIds := map[string]bool{}
+
+	for _, artist := range track.Extension.Track.AdditionalMetadata.Artists {
+		id := b.findArtistIdByMbid(ctx, subsonicUser, artist.MBID)
+		if id == "" {
+			return nil
+		}
+
+		artistIds[id] = true
+	}
+
+	trackParams := url.Values{
+		"artistCount": []string{"0"},
+		"albumCount":  []string{"0"},
+		"songCount":   []string{fallbackCount}, // I do not know what number is reasonable for multiple matches. Maybe I'll make it configurable
+		"query":       []string{track.Title},
+	}
+
+	resp, ok := b.makeSubsonicRequest(ctx, "search3", subsonicUser, &trackParams)
+	if !ok {
+		return nil
+	}
+
+	for _, subsonicTrack := range resp.Subsonic.SearchResult3.Song {
+		if subsonicTrack.Title == track.Title && len(artistIds) == len(subsonicTrack.Artists) {
+			missing := false
+
+			for _, artist := range subsonicTrack.Artists {
+				found := artistIds[artist.Id]
+
+				if !found {
+					missing = true
+				}
+			}
+
+			if !missing {
+				return &subsonicTrack
+			}
+		}
+	}
+
+	return nil
+}
+
+func (b *BrainzPlaylistPlugin) importPlaylist(
+	ctx context.Context,
+	source string,
+	playlistName string,
+	subsonicUser string,
+	playlists []overallPlaylist,
+	rating map[int32]bool,
+	fallbackCount string,
+) {
+	var id string
+	var err error
+	var listenBrainzPlaylist *lbPlaylist
+
+	for _, plsMetadata := range playlists {
+		if plsMetadata.Playlist.Identifier == source {
+			id = getIdentifier(plsMetadata.Playlist.Identifier)
+
+			listenBrainzPlaylist, err = b.getPlaylist(ctx, id)
+			break
+		}
+	}
+
+	if err != nil {
+		err = errors.Join(err)
+		log.Printf("Failed to fetch playlist %s for user %s: %v", id, subsonicUser, err)
+		return
+	} else if listenBrainzPlaylist == nil {
+		log.Printf("Failed to get playlist for user %s", subsonicUser)
+		return
+	}
+
+	songIds := []string{}
+
+	trackParams := url.Values{
+		"artistCount": []string{"0"},
+		"albumCount":  []string{"0"},
+		"songCount":   []string{"1"},
+	}
+
+	missing := []string{}
+	excluded := []string{}
+
+	for _, track := range listenBrainzPlaylist.Tracks {
+		mbid := getIdentifier(track.Identifier[0])
+		trackParams.Set("query", mbid)
+
+		resp, ok := b.makeSubsonicRequest(ctx, "search3", subsonicUser, &trackParams)
+		if !ok {
+			continue
+		}
+
+		var song *responses.Child
+
+		if len(resp.Subsonic.SearchResult3.Song) > 0 {
+			song = &resp.Subsonic.SearchResult3.Song[0]
+		} else {
+			song = b.fallbackLookup(ctx, subsonicUser, &track, fallbackCount)
+		}
+
+		if song != nil {
+			if rating[song.UserRating] {
+				songIds = append(songIds, song.Id)
+			} else {
+				excluded = append(excluded, fmt.Sprintf("%s by %s", track.Title, track.Creator))
+			}
+		} else {
+			missing = append(missing, fmt.Sprintf("%s by %s", track.Title, track.Creator))
+		}
+	}
+
+	resp, ok := b.makeSubsonicRequest(ctx, "getPlaylists", subsonicUser, &url.Values{})
+	if !ok {
+		return
+	}
+
+	existingPlaylist := b.findExistingPlaylist(resp, playlistName)
+	createPlaylistParams := url.Values{
+		"songId": songIds,
+	}
+
+	if existingPlaylist != nil {
+		createPlaylistParams.Add("playlistId", existingPlaylist.Id)
+	} else {
+		createPlaylistParams.Add("name", playlistName)
+	}
+
+	_, ok = b.makeSubsonicRequest(ctx, "createPlaylist", subsonicUser, &createPlaylistParams)
+	if !ok {
+		return
+	}
+
+	comment := fmt.Sprintf("%s&nbsp;\n%s", listenBrainzPlaylist.Annotation, listenBrainzPlaylist.Identifier)
+
+	if len(missing) > 0 {
+		comment += fmt.Sprintf("&nbsp;\nTracks not matched by MBID: %s", strings.Join(missing, ", "))
+	}
+
+	if len(excluded) > 0 {
+		comment += fmt.Sprintf("&nbsp;\nTracks excluded by rating rule: %s", strings.Join(excluded, ", "))
+	}
+
+	if existingPlaylist != nil && existingPlaylist.Comment != comment {
+		policy := bluemonday.StrictPolicy()
+		sanitized := html.UnescapeString(policy.Sanitize(comment))
+		updatePlaylistParams := url.Values{
+			"playlistId": []string{existingPlaylist.Id},
+			"comment":    []string{sanitized},
+		}
+
+		_, ok = b.makeSubsonicRequest(ctx, "updatePlaylist", subsonicUser, &updatePlaylistParams)
+		if !ok {
+			return
+		}
+	}
+
+	log.Printf("Successfully processed playlist %s for user %s", playlistName, subsonicUser)
+}
+
+func (b *BrainzPlaylistPlugin) updatePlaylists(ctx context.Context, conf map[string]string) (*api.SchedulerCallbackResponse, error) {
+	b.artistMbidToId = map[string]string{}
+
+	delimiter := conf["split"]
+	if delimiter == "" {
+		delimiter = ";"
+	}
+
+	subsonicUsers := strings.Split(conf["users"], delimiter)
+	sources := strings.Split(conf["sources"], delimiter)
+
+	var fallbackCount string
+	if conf["fallbackcount"] != "" {
+		fallbackCount = conf["fallbackcount"]
+	} else {
+		fallbackCount = "15"
+	}
+
+	for idx := range subsonicUsers {
+		allowedRatings := conf[fmt.Sprintf("rating[%d]", idx)]
+		var ratings map[int32]bool
+
+		if allowedRatings != "" {
+			ratings = map[int32]bool{}
+			for item := range strings.SplitSeq(allowedRatings, ",") {
+				rating, _ := strconv.Atoi(item)
+				ratings[int32(rating)] = true
+			}
+		} else {
+			ratings = map[int32]bool{0: true, 1: true, 2: true, 3: true, 4: true, 5: true}
+		}
+
+		lbzUser := conf[fmt.Sprintf("users[%d]", idx)]
+
+		playlists, err := b.getPlaylists(ctx, lbzUser)
+		if err != nil {
+			log.Printf("Failed to fetch playlists for user %s: %v", lbzUser, err)
+			continue
+		}
+
+		for sourceIdx, source := range sources {
+			plsName := conf[fmt.Sprintf("sources[%d]", sourceIdx)]
+			b.importPlaylist(ctx, source, plsName, subsonicUsers[idx], playlists, ratings, fallbackCount)
+		}
+	}
+
+	return &api.SchedulerCallbackResponse{}, nil
+}
+
+func (b *BrainzPlaylistPlugin) OnSchedulerCallback(ctx context.Context, req *api.SchedulerCallbackRequest) (*api.SchedulerCallbackResponse, error) {
+	configService := config.NewConfigService()
+
+	configResp, err := configService.GetPluginConfig(ctx, &config.GetPluginConfigRequest{})
+	if err != nil {
+		log.Printf("Failed to get plugin configuration: %v", err)
+		return &api.SchedulerCallbackResponse{Error: fmt.Sprintf("Config error: %v", err)}, nil
+	}
+
+	return b.updatePlaylists(ctx, configResp.Config)
+}
+
+func (b *BrainzPlaylistPlugin) initialFetch(
+	ctx context.Context,
+	sched scheduler.SchedulerService,
+	conf map[string]string,
+	users []string,
+	sources []string,
+) error {
+	now, err := sched.TimeNow(ctx, &scheduler.TimeNowRequest{})
+	if err != nil {
+		return err
+	}
+
+	nowTs, err := time.Parse(time.RFC3339Nano, now.Rfc3339Nano)
+	if err != nil {
+		return err
+	}
+
+	missing := false
+	olderThanThreeHours := false
+
+userLoop:
+	for _, user := range users {
+		playlistResp, ok := b.makeSubsonicRequest(ctx, "getPlaylists", user, &url.Values{})
+		if !ok {
+			return errors.New("Failed to fetch playlists on initial fetch")
+		}
+
+		for _, source := range sources {
+			pls := b.findExistingPlaylist(playlistResp, source)
+
+			if pls == nil {
+				missing = true
+				break userLoop
+			}
+
+			if nowTs.Sub(pls.Changed) > 3*time.Hour {
+				olderThanThreeHours = true
+				break userLoop
+			}
+		}
+	}
+
+	if missing || olderThanThreeHours {
+		log.Println("Missing or outdated playlists, fetching on initial sync")
+
+		playlistResp, err := b.updatePlaylists(ctx, conf)
+		if err != nil {
+			return err
+		}
+
+		if playlistResp.Error != "" {
+			return errors.New(playlistResp.Error)
+		}
+	} else {
+		log.Println("No missing/outdated playlists, not fetching on startup")
+	}
+
+	return nil
+}
+
+func (b *BrainzPlaylistPlugin) OnInit(ctx context.Context, req *api.InitRequest) (*api.InitResponse, error) {
+	conf := req.Config
+
+	schedule, ok := conf["schedule"]
+	if !ok {
+		schedule = "@every 24h"
+	}
+
+	delimiter := conf["split"]
+	if delimiter == "" {
+		delimiter = ";"
+	}
+
+	usersString := conf["users"]
+	if usersString == "" {
+		log.Printf("Missing required 'users' configuration")
+		return &api.InitResponse{Error: "Missing required 'users' configuration"}, nil
+	}
+
+	usersSplit := strings.Split(usersString, delimiter)
+	userOk := true
+
+	for idx := range usersSplit {
+		lbzUser := conf[fmt.Sprintf("users[%d]", idx)]
+
+		if lbzUser == "" {
+			userOk = false
+			log.Printf("User %s is missing a ListenBrainz username", usersSplit[idx])
+		}
+	}
+
+	if !userOk {
+		return &api.InitResponse{Error: "One or more users is missing a corresponding ListenBrainz username"}, nil
+	}
+
+	sourcesString := conf["sources"]
+	if sourcesString == "" {
+		log.Printf("Missing required 'sources' configuration")
+		return &api.InitResponse{Error: "Missing required 'sources' configuration"}, nil
+	}
+
+	sourcesSplit := strings.Split(sourcesString, delimiter)
+	sourcesOk := true
+	sourceNames := make([]string, len(sourcesSplit))
+
+	ratingOk := true
+
+	for idx := range sourcesSplit {
+		mappedName := conf[fmt.Sprintf("sources[%d]", idx)]
+
+		if mappedName == "" {
+			sourcesOk = false
+			log.Printf("Source %s is missing a playlist name", sourcesSplit[idx])
+		} else {
+			sourceNames[idx] = mappedName
+		}
+
+		rating := conf[fmt.Sprintf("rating[%d]", idx)]
+		if rating != "" {
+			for item := range strings.SplitSeq(rating, ",") {
+				_, err := strconv.Atoi(item)
+				if err != nil {
+					ratingOk = false
+				}
+			}
+		}
+	}
+
+	if !sourcesOk {
+		return &api.InitResponse{Error: "One or more sources is missing a corresponding playlist name"}, nil
+	}
+
+	if !ratingOk {
+		return &api.InitResponse{Error: "One or more users has misconfigured `rating`. This must be a comma-separated list of ratings"}, nil
+	}
+
+	if conf["fallbackcount"] != "" {
+		value, err := strconv.Atoi(conf["fallbackcount"])
+		if err != nil {
+			return &api.InitResponse{Error: "`FallbackCount` is not a valid number"}, nil
+		}
+
+		if value < 1 || value > 500 {
+			return &api.InitResponse{Error: "`FallbackCount` must be between 1 and 500 (inclusive)"}, nil
+		}
+	}
+
+	// SchedulerService instance for scheduling tasks.
+	schedService := scheduler.NewSchedulerService()
+
+	_, err := schedService.ScheduleRecurring(ctx, &scheduler.ScheduleRecurringRequest{
+		CronExpression: schedule,
+		ScheduleId:     peridiocSyncId,
+	})
+
+	if err != nil {
+		msg := fmt.Sprintf("Failed to schedule playlist sync. Is your schedule a valid cron expression? %v", err)
+		log.Println(msg)
+		return &api.InitResponse{Error: msg}, nil
+	}
+
+	if conf["checkonstartup"] != "false" {
+		err := b.initialFetch(ctx, schedService, conf, usersSplit, sourceNames)
+		if err != nil {
+			log.Println("Failed to do initial sync. Proceeding anyway %w", err)
+		}
+	}
+
+	log.Println("init success")
+
+	return &api.InitResponse{}, nil
+}
+
+func main() {}
+
+func init() {
+	// Configure logging: No timestamps, no source file/line, prepend
+	log.SetFlags(0)
+	log.SetPrefix("[LBZ Playlist Fetcher]")
+
+	api.RegisterLifecycleManagement(&BrainzPlaylistPlugin{})
+	api.RegisterSchedulerCallback(&BrainzPlaylistPlugin{})
+}
